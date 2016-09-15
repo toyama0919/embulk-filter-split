f42d7905a6db9d7beb00f2180b0045c93a1ebf20
toyama0919 <toyama0919@gmail.com>
2016-09-15 11:56:55 +0900

    fix json array split column.

--- src/main/java/org/embulk/filter/split/SplitFilterPlugin.java
+++ src/main/java/org/embulk/filter/split/SplitFilterPlugin.java
@@ -1,5 +1,8 @@
 package org.embulk.filter.split;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
@@ -15,9 +18,11 @@ import org.embulk.spi.PageOutput;
 import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
 import org.embulk.spi.type.Types;
+import org.msgpack.value.Value;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
 public class SplitFilterPlugin
         implements FilterPlugin
@@ -29,6 +34,10 @@ public class SplitFilterPlugin
         @ConfigDefault("true")
         public boolean getKeepInput();
 
+        @Config("skip_null_or_empty")
+        @ConfigDefault("true")
+        public boolean getSkipNullOrEmpty();
+
         @Config("delimiter")
         @ConfigDefault("\",\"")
         public String getDelimiter();
@@ -37,8 +46,7 @@ public class SplitFilterPlugin
         public String getTargetKey();
 
         @Config("output_key")
-        @ConfigDefault("null")
-        public Optional<String> getOutputKey();
+        public String getOutputKey();
     }
 
     @Override
@@ -55,10 +63,9 @@ public class SplitFilterPlugin
                 builder.add(outputColumn);
             }
         }
-        if (task.getOutputKey().isPresent()) {
-            Column outputColumn = new Column(i++, task.getOutputKey().get(), Types.STRING);
-            builder.add(outputColumn);
-        }
+        Column outputColumn = new Column(i++, task.getOutputKey(), Types.STRING);
+        builder.add(outputColumn);
+
         Schema outputSchema = new Schema(builder.build());
         control.run(task.dump(), outputSchema);
     }
@@ -69,9 +76,7 @@ public class SplitFilterPlugin
     {
         final PluginTask task = taskSource.loadTask(PluginTask.class);
         final Column targetColumn = inputSchema.lookupColumn(task.getTargetKey());
-        final Column outputColumn = task.getOutputKey().isPresent() ?
-            outputSchema.lookupColumn(task.getOutputKey().get()) :
-            outputSchema.lookupColumn(task.getTargetKey());
+        final Column outputColumn = outputSchema.lookupColumn(task.getOutputKey());
 
         return new PageOutput() {
             private PageReader reader = new PageReader(inputSchema);
@@ -94,38 +99,92 @@ public class SplitFilterPlugin
             {
                 reader.setPage(page);
                 while (reader.nextRecord()) {
-                    String[] words = StringUtils.split(reader.getString(targetColumn), task.getDelimiter());
-                    for (String word : words) {
-                        for (Column column : outputSchema.getColumns()) {
-                            if (column.getName().equals(outputColumn.getName())) {
-                                builder.setString(outputColumn, word);
-                                continue;
-                            }
-                            if (reader.isNull(column)) {
-                                builder.setNull(column);
-                                continue;
-                            }
-                            if (Types.STRING.equals(column.getType())) {
-                                builder.setString(column, reader.getString(column));
-                            }
-                            else if (Types.BOOLEAN.equals(column.getType())) {
-                                builder.setBoolean(column, reader.getBoolean(column));
-                            }
-                            else if (Types.DOUBLE.equals(column.getType())) {
-                                builder.setDouble(column, reader.getDouble(column));
-                            }
-                            else if (Types.LONG.equals(column.getType())) {
-                                builder.setLong(column, reader.getLong(column));
-                            }
-                            else if (Types.TIMESTAMP.equals(column.getType())) {
-                                builder.setTimestamp(column, reader.getTimestamp(column));
-                            }
-                            else if (Types.JSON.equals(column.getType())) {
-                                builder.setJson(column, reader.getJson(column));
-                            }
+                    List<String> words = getWords(task, targetColumn);
+                    if (reader.isNull(targetColumn) || words.isEmpty()) {
+                        if (task.getSkipNullOrEmpty()) {
+                            continue;
                         }
+                        setColumns(outputSchema, outputColumn, null);
                         builder.addRecord();
                     }
+                    else {
+                        for (String word : words) {
+                            setColumns(outputSchema, outputColumn, word);
+                            builder.addRecord();
+                        }
+                    }
+                }
+            }
+
+            /**
+             * @param task
+             * @param targetColumn
+             * @return
+             */
+            private List<String> getWords(final PluginTask task, final Column targetColumn)
+            {
+                List<String> words = Lists.newArrayList();
+                if (targetColumn.getType().equals(Types.STRING)) {
+                    words = Arrays.asList(StringUtils.split(reader.getString(targetColumn), task.getDelimiter()));
+                }
+                else if (targetColumn.getType().equals(Types.JSON)) {
+                    final Value json = reader.getJson(targetColumn);
+                    if (json.isArrayValue()) {
+                        for (Value value : json.asArrayValue().list()) {
+                            words.add(value.toString());
+                        }
+                    }
+                }
+                return words;
+            }
+
+            /**
+             * @param outputSchema
+             * @param outputColumn
+             * @param word
+             */
+            private void setColumns(final Schema outputSchema, final Column outputColumn, String word)
+            {
+                for (Column column : outputSchema.getColumns()) {
+                    if (column.getName().equals(outputColumn.getName())) {
+                        if (word == null) {
+                            builder.setNull(outputColumn);
+                        }
+                        else {
+                            builder.setString(outputColumn, word);
+                        }
+                        continue;
+                    }
+                    setKeepColumns(column);
+                }
+            }
+
+            /**
+             * @param column
+             */
+            private void setKeepColumns(Column column)
+            {
+                if (reader.isNull(column)) {
+                    builder.setNull(column);
+                    return;
+                }
+                if (Types.STRING.equals(column.getType())) {
+                    builder.setString(column, reader.getString(column));
+                }
+                else if (Types.BOOLEAN.equals(column.getType())) {
+                    builder.setBoolean(column, reader.getBoolean(column));
+                }
+                else if (Types.DOUBLE.equals(column.getType())) {
+                    builder.setDouble(column, reader.getDouble(column));
+                }
+                else if (Types.LONG.equals(column.getType())) {
+                    builder.setLong(column, reader.getLong(column));
+                }
+                else if (Types.TIMESTAMP.equals(column.getType())) {
+                    builder.setTimestamp(column, reader.getTimestamp(column));
+                }
+                else if (Types.JSON.equals(column.getType())) {
+                    builder.setJson(column, reader.getJson(column));
                 }
             }
         };
